b813f80d01aafc2b00202a8ae3986bb4df8ab947
enzodiaz <enzodiazdev@gmail.com>
2021-09-11 16:04:39 -0300

    implementa discord api types y se eliminan los tipos sin uso

--- src/interactions/InteractionType.ts
+++
@@ -1,7 +0,0 @@
-enum InteractionType {
-    PING = 1,
-    APPLICATION_COMMAND = 2,
-    MESSAGE_COMPONENT = 3
-}
-
-export default InteractionType;
--- src/message_components/ActionRow.ts
+++
@@ -1,5 +0,0 @@
-import ComponentType from "../interactions/ComponentType";
-
-export default interface ActionRow {
-    type: ComponentType.ACTION_ROW
-}
